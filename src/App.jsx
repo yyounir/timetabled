9174a67d93469c1f6357d8dbcc7768c0eae856a7
yyounir <yyproton168@icloud.com>
2026-09-17 21:33:39 -0400

    0.1 - Init repository

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>TimeTable</h1>  
+      <h2>Set Schedule</h2>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/calendar.jsx
@@ -0,0 +1,9 @@
+const Calendar = () =>{
+    return (
+        <div className = "Calendar">
+            Testing the calendar component
+        </div>
+    )
+}
+
+export default Calendar
